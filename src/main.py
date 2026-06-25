36f44cb1b43cf2f2209d23759584509d669e2ef2
Juan Torres <jmmtorres1991@yahoo.com>
2026-06-24 19:47:44 -0500

    home this works

--- src/main.py
+++ src/main.py
@@ -3,13 +3,16 @@ from parent_node import ParentNode
 from leaf_node import LeafNode
 import copystatic as copy
 import gencontent as gc
+import sys
 
 
 def main():
+    nsrc = sys.argv[0]
+    print(nsrc)
     copy.clean_dir("public")
     copy.copy_static("static","public")
     #gc.generate_page("content/index.md","template.html","public/index.html")
-    gc.generate_pages_recursive("content","template.html","public")
+    #gc.generate_pages_recursive("content","template.html","public")
 
 
 main()
